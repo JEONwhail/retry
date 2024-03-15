145d2a0e40b220c3c2ab36193bbdd59d1094c6d1
JEONwhail <green_jeon247>
2024-03-15 11:10:36 +0900

    Q&A 드롭다운,업 수정

--- retry/JS/script.js
+++ retry/JS/script.js
@@ -1,21 +1,19 @@
 
-const questions = document.querySelectorAll('.qna .dl_box .question');
-const answers = document.querySelectorAll('.qna .dl_box .answer');
+const question = document.querySelectorAll('.qna .dl_box .question');
+const answer = document.querySelectorAll('.qna .dl_box .answer');
 
-questions.forEach(function (question) {
+question.forEach(function (question) {
   question.addEventListener('click', function () {
-    answers.style.display = 'block' ;
+    answer.forEach(answer => {
+      answer
+      answer.style.display = 'block' ;
+    });
   });
 });
 
-// question.addEventListener('click', function () {
-//   // answer.style.display = 'block';
-//   console.log('하이');
-// });
-
-// answer.forEach(function (answer) {
-//   answer.addEventListener('click', function () {
-//     answer.style.display = 'none' ;
-//   });
-// });
+answer.forEach(function (answer) {
+  answer.addEventListener('click', function () {
+    answer.style.display = 'none' ;
+  });
+});
 
