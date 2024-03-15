e90cb1dcb1c09abcc4df992d805428eb3db7404c
JEONwhail <green_jeon247>
2024-03-15 14:38:01 +0900

    board완성

--- retry/JS/script.js
+++ retry/JS/script.js
@@ -1,18 +1,48 @@
+// BOARD
+const defalts = document.querySelectorAll('.board .board-line .defalt_board');
+const hiddens = document.querySelectorAll('.board .board-line .hidden_board');
+console.log(defalts);
+console.log(hiddens[1]);
 
-const question = document.querySelectorAll('.qna .dl_box .question');
-const answer = document.querySelectorAll('.qna .dl_box .answer');
-
-question.forEach(function (question) {
-  question.addEventListener('click', function () {
-    answer.forEach(answer => {
-      answer
-      answer.style.display = 'block' ;
-    });
+defalts.forEach(function (defalt, dIndex) {
+  defalt.addEventListener('click', function () {
+    console.log(dIndex);
+    console.log(hiddens[dIndex]);
+    hiddens[dIndex].style.display = 'block';
   });
 });
 
-answer.forEach(function (answer) {
+hiddens.forEach(function (hidden) {
+  hidden.addEventListener('click', function () {
+    console.log(hidden);
+    hidden.style.display = 'none' ;
+  });
+});
+
+
+// Q&A
+const questions = document.querySelectorAll('.qna .dl_box .question');
+const answers = document.querySelectorAll('.qna .dl_box .answer');
+console.log(questions);
+console.log(answers[1]);
+
+questions.forEach(function (question, qIndex) {
+  question.addEventListener('click', function () {
+
+    console.log(qIndex);
+    console.log(answers[qIndex]);
+    answers[qIndex].style.display = 'block';
+
+    // answers.forEach(answer => {
+    //   answer
+    //   answer.style.display = 'block' ;
+    // });
+  });
+});
+
+answers.forEach(function (answer) {
   answer.addEventListener('click', function () {
+    console.log(answer);
     answer.style.display = 'none' ;
   });
 });
