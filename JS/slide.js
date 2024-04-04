8544453e275b0865584b0455d6c9581124abfc99
JEONwhail <green_jeon247>
2024-04-04 19:54:26 +0900

    슬라이드 실패

--- JS/slide.js
+++ JS/slide.js
@@ -1,11 +1,34 @@
+// let swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   loop : true, 
+//   autoPlay : {
+//     delay:300
+//   },
+//   coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 50,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// })
+// ;
+
 let swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  loop : true, 
-  autoPlay : {
-    delay:300
+  loop: true, 
+  autoplay: {
+    delay: 3000, // 3000밀리초(3초)마다 슬라이드가 전환됩니다.
+    disableOnInteraction: false, // 사용자 상호 작용 후에도 자동 재생을 계속합니다.
   },
   coverflowEffect: {
     rotate: 50,
@@ -17,5 +40,5 @@ let swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
   },
-})
-;
+});
+
