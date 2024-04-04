7e240894170e94cd3687fb7a76873c12b29b4817
JEONwhail <green_jeon247>
2024-04-04 19:33:24 +0900

    로딩페이지 작업 완료 폴더 정리

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
+window.onload = function () {
+  document.querySelector('.loading').style.visibility = 'hidden'
+  document.querySelector('.loading').style.opacity = 0
+}
+
+
 // BOARD
 const defalts = document.querySelectorAll('.board .board-line .defalt_board');
 const hiddens = document.querySelectorAll('.board .board-line .hidden_board');
@@ -21,29 +27,55 @@ hiddens.forEach(function (hidden) {
 
 
 // Q&A
+
+//하나 열리고 하나열리고 다 열림
+// const questions = document.querySelectorAll('.qna .dl_box .question');
+// const answers = document.querySelectorAll('.qna .dl_box .answer');
+// console.log(questions);
+// console.log(answers[1]);
+
+// questions.forEach(function (question, qIndex) {
+//   question.addEventListener('click', function () {
+
+//     console.log(qIndex);
+//     console.log(answers[qIndex]);
+//     answers[qIndex].style.display = 'block';
+
+//     // answers.forEach(answer => {
+//     //   answer
+//     //   answer.style.display = 'block' ;
+//     // });
+//   });
+// });
+
+// answers.forEach(function (answer) {
+//   answer.addEventListener('click', function () {
+//     console.log(answer);
+//     answer.style.display = 'none' ;
+//   });
+// });
+
 const questions = document.querySelectorAll('.qna .dl_box .question');
 const answers = document.querySelectorAll('.qna .dl_box .answer');
-console.log(questions);
-console.log(answers[1]);
 
 questions.forEach(function (question, qIndex) {
   question.addEventListener('click', function () {
 
-    console.log(qIndex);
-    console.log(answers[qIndex]);
-    answers[qIndex].style.display = 'block';
+    answers.forEach(function (answer, aIndex) {
+      if (qIndex === aIndex) {
 
-    // answers.forEach(answer => {
-    //   answer
-    //   answer.style.display = 'block' ;
-    // });
+        answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+      } else {
+
+        answer.style.display = 'none';
+      }
+    });
   });
 });
 
 answers.forEach(function (answer) {
   answer.addEventListener('click', function () {
-    console.log(answer);
-    answer.style.display = 'none' ;
+    answer.style.display = 'none';
   });
 });
 
